66b7d858a58adb966994a490959cb3a0c95ade62
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-09-20 17:28:45 +0200

    Initial commit

---
+++ src/main/java/com/example/functionalBookstore/FunctionalBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.functionalBookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalBookstoreApplication.class, args);
+	}
+
+}
