88aa245b534cd46f4e77fae3deb418cbe26cdac3
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-10-24 15:49:33 +0200

    Add User Repository

---
+++ src/main/java/com/example/functionalBookstore/domain/user/infrastructure/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.functionalBookstore.domain.user.infrastructure;
+
+import com.example.functionalBookstore.domain.user.core.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
