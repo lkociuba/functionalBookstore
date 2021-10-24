402e1a1f2d56e145deb461757b6d52a808d5c05b
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-10-24 17:40:02 +0200

    Add UserDatabaseAdapter with save

---
+++ src/main/java/com/example/functionalBookstore/domain/user/infrastructure/UserDatabaseAdapter.java
@@ -0,0 +1,16 @@
+package com.example.functionalBookstore.domain.user.infrastructure;
+
+import com.example.functionalBookstore.domain.user.core.model.User;
+import com.example.functionalBookstore.domain.user.core.ports.ioutgoing.UserDatabase;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserDatabaseAdapter implements UserDatabase {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
