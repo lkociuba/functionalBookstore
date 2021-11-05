3dddbcf18a768586cf170c45dab49a6c3b538174
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-11-05 06:20:39 +0100

    Add Adapter to find user by email
    Refactor name  of interface.

--- src/main/java/com/example/functionalBookstore/domain/user/core/ports/ioutgoing/FindUserByEmailAddress.java
+++ src/main/java/com/example/functionalBookstore/domain/user/core/ports/ioutgoing/FindUserByEmailAddress.java
@@ -2,6 +2,6 @@ package com.example.functionalBookstore.domain.user.core.ports.ioutgoing;
 
 import com.example.functionalBookstore.domain.user.core.model.User;
 
-public interface FindUserByEmail {
-    User user(String email);
+public interface FindUserByEmailAddress {
+    User user(String emailAddress);
 }
