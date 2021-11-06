b89a53309ea28848f85a5f47980f6c96ab97c753
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-11-06 15:17:18 +0100

    Add Spring Security Service as MockBean

--- src/test/java/com/example/functionalBookstore/application/FunctionalBookstoreApplicationControllerTest.java
+++ src/test/java/com/example/functionalBookstore/application/FunctionalBookstoreApplicationControllerTest.java
@@ -1,9 +1,11 @@
 package com.example.functionalBookstore.application;
 
+import com.example.functionalBookstore.domain.user.infrastructure.UserSpringSecurityService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,6 +18,9 @@ class FunctionalBookstoreApplicationControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private UserSpringSecurityService securityService;
 
     @Test
     void shouldShowWelcomePage() throws Exception {
