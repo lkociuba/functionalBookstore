fecef65954478c09f7d798dd4eeb2ef9c927ddc2
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-11-10 19:26:11 +0100

    Add books to database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,3 +20,22 @@ INSERT INTO USERS_ROLES (user_id, role_id)
 values (1003, 2003);
 INSERT INTO USERS_ROLES (user_id, role_id)
 values (1003, 2004);
+
+INSERT INTO BOOK (book_id, name, description, price, created_time)
+values (40000, 'Red book', 'Book about red things.', '9.99', sysdate);
+INSERT INTO BOOK (book_id, name, description, price, created_time)
+values (40001, 'Green book', 'Book about green things.', '19.99', sysdate);
+INSERT INTO BOOK (book_id, name, description, price, created_time)
+values (40002, 'Yellow book', 'Book about yellow things.', '29.99', sysdate);
+INSERT INTO BOOK (book_id, name, description, price, created_time)
+values (40003, 'Brown book', 'Book about brown things.', '39.99', sysdate);
+INSERT INTO BOOK (book_id, name, description, price, created_time)
+values (40004, 'Grey book', 'Book about grey things.', '49.99', sysdate);
+INSERT INTO BOOK (book_id, name, description, price, created_time)
+values (40005, 'Orange book', 'Book about orange things.', '59.99', sysdate);
+INSERT INTO BOOK (book_id, name, description, price, created_time)
+values (40006, 'Violet book', 'Book about violet things.', '69.99', sysdate);
+INSERT INTO BOOK (book_id, name, description, price, created_time)
+values (40007, 'Geographical atlas', 'A simple atlas for beginners.', '79.99', sysdate);
+INSERT INTO BOOK (book_id, name, description, price, created_time)
+values (40008, 'Fantasy book', 'Never Ending story.', '89.99', sysdate);
