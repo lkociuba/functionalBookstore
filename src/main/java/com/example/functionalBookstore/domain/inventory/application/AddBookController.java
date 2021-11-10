01c4afc097cb5f36215c7d48a3540f9d752d6512
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-11-10 18:45:24 +0100

    Add AddBookController +page +config

--- src/main/java/com/example/functionalBookstore/domain/inventory/application/AddBookController.java
+++ src/main/java/com/example/functionalBookstore/domain/inventory/application/AddBookController.java
@@ -1,9 +1,12 @@
 package com.example.functionalBookstore.domain.inventory.application;
 
+import com.example.functionalBookstore.domain.inventory.core.model.BookAddCommand;
 import com.example.functionalBookstore.domain.inventory.core.ports.incoming.AddNewBook;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,7 +17,18 @@ public class AddBookController {
     private final AddNewBook addNewBook;
 
     @GetMapping
-    public String showAddNewBookPage(){
+    public String showAddNewBookPage() {
         return "addBook";
+    }
+
+    @ModelAttribute("book")
+    public BookAddCommand bookAddCommand() {
+        return new BookAddCommand();
+    }
+
+    @PostMapping
+    public String addNewBook(@ModelAttribute("book") BookAddCommand bookAddCommand){
+        addNewBook.save(bookAddCommand);
+        return "redirect:/addBook?success";
     }
 }
---
+++ src/main/java/com/example/functionalBookstore/infrastructure/InventoryDomainConfig.java
@@ -0,0 +1,24 @@
+package com.example.functionalBookstore.infrastructure;
+
+import com.example.functionalBookstore.domain.inventory.core.BookService;
+import com.example.functionalBookstore.domain.inventory.core.model.Book;
+import com.example.functionalBookstore.domain.inventory.core.ports.incoming.AddNewBook;
+import com.example.functionalBookstore.domain.inventory.core.ports.outgoing.BookDatabase;
+import com.example.functionalBookstore.domain.inventory.infrastructure.BookDatabaseAdapter;
+import com.example.functionalBookstore.domain.inventory.infrastructure.BookRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InventoryDomainConfig {
+
+    @Bean
+    public BookDatabase bookDatabase(BookRepository bookRepository){
+        return new BookDatabaseAdapter(bookRepository);
+    }
+
+    @Bean
+    public AddNewBook addNewBook(BookDatabase bookDatabase){
+        return new BookService(bookDatabase);
+    }
+}
