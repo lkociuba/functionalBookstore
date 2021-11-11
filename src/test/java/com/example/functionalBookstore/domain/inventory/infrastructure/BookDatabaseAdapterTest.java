6cf4dad0b2c88bafd86621dda9d8db58bf6ff2b4
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-11-11 17:26:39 +0100

    Refactor BookDatabaseAdapterTest

--- src/test/java/com/example/functionalBookstore/domain/inventory/infrastructure/BookDatabaseAdapterTest.java
+++ src/test/java/com/example/functionalBookstore/domain/inventory/infrastructure/BookDatabaseAdapterTest.java
@@ -1,30 +1,19 @@
 package com.example.functionalBookstore.domain.inventory.infrastructure;
 
 import com.example.functionalBookstore.domain.inventory.core.model.Book;
-import com.example.functionalBookstore.domain.user.infrastructure.UserDatabaseAdapter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
-
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.*;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -58,7 +47,7 @@ class BookDatabaseAdapterTest {
     void shouldGetEmptyOptionalFromNoDatabaseConnection() {
         //when
         Optional<Page<Book>> result = bookDatabaseAdapter.
-                getBooksPaginatedAndSorted(1,2,"name", "asc");
+                getBooksPaginatedAndSorted(1, 2, "name", "asc");
 
         //then
         assertThat(result, is(Optional.empty()));
