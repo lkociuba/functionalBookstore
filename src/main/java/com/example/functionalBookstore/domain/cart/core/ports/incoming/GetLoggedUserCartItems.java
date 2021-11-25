4a78bb16a0c0ab90c0c0253a791c89642cf8f2a9
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-11-25 16:21:40 +0100

    Add GetLoggedUserCartItems port

---
+++ src/main/java/com/example/functionalBookstore/domain/cart/core/ports/incoming/GetLoggedUserCartItems.java
@@ -0,0 +1,10 @@
+package com.example.functionalBookstore.domain.cart.core.ports.incoming;
+
+import com.example.functionalBookstore.domain.cart.core.model.CartItem;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GetLoggedUserCartItems {
+    Optional<List<CartItem>> handle();
+}
