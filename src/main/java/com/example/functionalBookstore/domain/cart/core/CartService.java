4d8c83d9c7f4d7603744a03bb0c572bcd4f874f1
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-11-25 17:46:12 +0100

    Add CartService

---
+++ src/main/java/com/example/functionalBookstore/domain/cart/core/CartService.java
@@ -0,0 +1,24 @@
+package com.example.functionalBookstore.domain.cart.core;
+
+import com.example.functionalBookstore.domain.cart.core.model.CartItem;
+import com.example.functionalBookstore.domain.cart.core.ports.incoming.GetLoggedUserCartItems;
+import com.example.functionalBookstore.domain.cart.core.ports.outgoing.CartItemDatabase;
+import com.example.functionalBookstore.domain.user.core.ports.incoming.GetLoggedUser;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class CartService implements GetLoggedUserCartItems {
+
+    private final CartItemDatabase cartItemDatabase;
+
+    private final GetLoggedUser getLoggedUser;
+
+    @Override
+    public Optional<List<CartItem>> handle() {
+        var loggedUserId = getLoggedUser.getLoggedUser().get().getId();
+        return cartItemDatabase.findCartItemsByUser(loggedUserId);
+    }
+}
