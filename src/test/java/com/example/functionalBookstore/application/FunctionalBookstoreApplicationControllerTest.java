38cc4b6bc1a5b05b056d00b816099f0a63261aff
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-11-28 14:53:59 +0100

    Add Mock User login to test

--- src/test/java/com/example/functionalBookstore/application/FunctionalBookstoreApplicationControllerTest.java
+++ src/test/java/com/example/functionalBookstore/application/FunctionalBookstoreApplicationControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,6 +24,7 @@ class FunctionalBookstoreApplicationControllerTest {
     private UserSpringSecurityService securityService;
 
     @Test
+    @WithMockUser(username = "manager@email")
     void shouldShowWelcomePage() throws Exception {
         mockMvc.perform(get("/")).andExpect(status().isOk());
     }
