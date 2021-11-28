b5d92820d479bd0e6538f5fd1c5786bf78f7ca1a
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-11-28 17:47:11 +0100

    Refactor GetBookById

--- src/main/java/com/example/functionalBookstore/domain/inventory/core/ports/incoming/GetBookById.java
+++ src/main/java/com/example/functionalBookstore/domain/inventory/core/ports/incoming/GetBookById.java
@@ -2,8 +2,6 @@ package com.example.functionalBookstore.domain.inventory.core.ports.incoming;
 
 import com.example.functionalBookstore.domain.inventory.core.model.Book;
 
-import java.util.Optional;
-
 public interface GetBookById {
-    Optional<Book> getBookById(Long bookId);
+    Book getBookById(Long bookId);
 }
