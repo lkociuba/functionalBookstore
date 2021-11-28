d7af1a675bdc6daa2aff42ccb84f1adf47ead914
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-11-28 15:22:34 +0100

    Delete AdNewCartItem port and command

--- src/main/java/com/example/functionalBookstore/domain/cart/core/ports/incoming/AddNewCartItem.java
+++
@@ -1,7 +0,0 @@
-package com.example.functionalBookstore.domain.cart.core.ports.incoming;
-
-import com.example.functionalBookstore.domain.cart.core.model.AddCartItemCommand;
-
-public interface AddNewCartItem {
-    void save(AddCartItemCommand addCartItemCommand);
-}
