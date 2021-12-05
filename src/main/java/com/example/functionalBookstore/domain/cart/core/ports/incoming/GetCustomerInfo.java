d509945cdb2af98136cea7ae5b96b9882c5feeaf
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-12-05 14:30:31 +0100

    Add GetCustomerInfo

---
+++ src/main/java/com/example/functionalBookstore/domain/cart/core/ports/incoming/GetCustomerInfo.java
@@ -0,0 +1,7 @@
+package com.example.functionalBookstore.domain.cart.core.ports.incoming;
+
+import com.example.functionalBookstore.domain.cart.core.model.CustomerInfo;
+
+public interface GetCustomerInfo {
+    CustomerInfo getCustomerInfo();
+}
