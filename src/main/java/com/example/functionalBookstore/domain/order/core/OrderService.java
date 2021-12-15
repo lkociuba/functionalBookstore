40984937b5d7cb44e81b7082eb976c76068fa6a7
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-12-15 20:17:48 +0100

    Refactor OrderService

--- src/main/java/com/example/functionalBookstore/domain/order/core/OrderService.java
+++ src/main/java/com/example/functionalBookstore/domain/order/core/OrderService.java
@@ -1,12 +1,18 @@
 package com.example.functionalBookstore.domain.order.core;
 
+import com.example.functionalBookstore.domain.cart.core.model.CartItem;
+import com.example.functionalBookstore.domain.cart.core.ports.incoming.DeleteCartItemAndCustomerInfoAfterOrderSave;
 import com.example.functionalBookstore.domain.cart.core.ports.incoming.GetCartFinalAmount;
 import com.example.functionalBookstore.domain.cart.core.ports.incoming.GetCustomerInfo;
+import com.example.functionalBookstore.domain.cart.core.ports.incoming.GetLoggedUserCartItems;
+import com.example.functionalBookstore.domain.inventory.core.model.Book;
 import com.example.functionalBookstore.domain.order.core.model.AddOrderCommand;
 import com.example.functionalBookstore.domain.order.core.model.Order;
+import com.example.functionalBookstore.domain.order.core.model.OrderItem;
 import com.example.functionalBookstore.domain.order.core.ports.incoming.AddNewOrder;
 import com.example.functionalBookstore.domain.order.core.ports.incoming.GetOrders;
 import com.example.functionalBookstore.domain.order.core.ports.outgoing.OrderDatabase;
+import com.example.functionalBookstore.domain.order.infrastructure.OrderItemRepository;
 import com.example.functionalBookstore.domain.order.infrastructure.OrderRepository;
 import com.example.functionalBookstore.domain.user.core.ports.incoming.GetLoggedUser;
 import lombok.RequiredArgsConstructor;
@@ -25,19 +31,18 @@ public class OrderService implements AddNewOrder, GetOrders {
     private final GetCartFinalAmount getCartFinalAmount;
     private final GetCustomerInfo getCustomerInfo;
     private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final GetLoggedUserCartItems getLoggedUserCartItems;
+    private final DeleteCartItemAndCustomerInfoAfterOrderSave deleteCartItemAndCustomerInfoAfterOrderSave;
 
     @Override
     public void saveOrder() {
-        var addOrderCommand = new AddOrderCommand()
-                .setUser(getLoggedUser.getLoggedUser())
-                .setOrderNumber(this.newOrderNumber())
-                .setCalculatedPrice(transformPriceToBigDecimal.apply(getCartFinalAmount.calculateCartFinalAmount()))
-                .setCustomerInfo(getCustomerInfo.getCustomerInfo());
-
-        var order = addOrderCommand.getOrder();
+        var order = this.createNewOrder();
         orderRepository.save(order);
 
+        this.saveOrderItemsToOrder(order);
 
+        deleteCartItemAndCustomerInfoAfterOrderSave.deleteCartItemAndCustomerInfoAfterOrderSave();
     }
 
     @Override
@@ -47,9 +52,28 @@ public class OrderService implements AddNewOrder, GetOrders {
 
     private final Function<Double, BigDecimal> transformPriceToBigDecimal = BigDecimal::valueOf;
 
+    private Order createNewOrder() {
+        var addOrderCommand = new AddOrderCommand()
+                .setUser(getLoggedUser.getLoggedUser())
+                .setOrderNumber(this.newOrderNumber())
+                .setCalculatedPrice(transformPriceToBigDecimal.apply(getCartFinalAmount.calculateCartFinalAmount()))
+                .setCustomerInfo(getCustomerInfo.getCustomerInfo());
+
+        return addOrderCommand.getOrder();
+    }
+
+    private void saveOrderItemsToOrder(Order order) {
+        var loggedUserCartItems = getLoggedUserCartItems.getLoggedUserCartItems();
+        for (CartItem item : loggedUserCartItems) {
+            var book = item.getBook();
+            var orderItem = new OrderItem(order, book);
+            orderItemRepository.save(orderItem);
+        }
+    }
+
     private int newOrderNumber() {
         var orderList = orderDatabase.findAllOrders();
-        if (orderList.isEmpty()){
+        if (orderList.isEmpty()) {
             return 1;
         }
         var maxOrderNumber = orderList.get().stream()
