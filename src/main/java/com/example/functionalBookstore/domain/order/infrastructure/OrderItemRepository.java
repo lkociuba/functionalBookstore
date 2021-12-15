31c626332bacd79616321027ffa329e534a01261
Łukasz Kociuba <kociubalukasz@gmail.com>
2021-12-15 19:54:17 +0100

    Add OrderItemRepository

---
+++ src/main/java/com/example/functionalBookstore/domain/order/infrastructure/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.example.functionalBookstore.domain.order.infrastructure;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends CrudRepository {
+}
